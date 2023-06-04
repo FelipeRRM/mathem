b74c00f6b2adacd9b29227571cea67f21f212815
Felipe <felipe_rrm@hotmail.com>
2023-06-04 16:45:36 +0200

    - Updated project to use latest Gradle and build tools compatible with Android Flamingo

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,9 +1,12 @@
+import org.gradle.api.JavaVersion
+
 object Config {
     const val appId = "com.feliperrm.mathem"
     const val minSdk = 24
     const val targetSdk = 33
     const val versionCode = 999999
     const val versionName = "1.0"
-    const val jvmTarget = "11"
+    val javaVersion = JavaVersion.VERSION_17
+    const val jvmTarget = "17"
     const val kotlinCompilerExtensionVersion = "1.4.7"
 }
